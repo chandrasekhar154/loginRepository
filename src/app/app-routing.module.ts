403c4f21249e0682d85ee2deb53a073c833f688d
chandrasekhar154 <chandrasekhar648@gmail.com>
2020-02-26 22:29:21 +0530

    generate ticket component was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { PfizerHomeComponent} from './pfizer-home/pfizer-home.component';
 import { PfizerLoginComponent} from './pfizer-login/pfizer-login.component';
 import { PfizerPackOrderComponent } from './pfizer-pack-order/pfizer-pack-order.component';
+import { GenerateTicketComponent } from './generate-ticket/generate-ticket.component';
 
 const routes: Routes = [
   { path: '', component: PfizerLoginComponent },
-  { path: 'pfizer-home', component: PfizerHomeComponent},
-  { path: 'pfizer-pack-order', component: PfizerPackOrderComponent},
+  { path: 'pfizer-home', component: PfizerHomeComponent },
+  { path: 'pfizer-pack-order', component: PfizerPackOrderComponent },
+  { path: 'generate-ticket', component: GenerateTicketComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { PfizerHomeComponent } from './pfizer-home/pfizer-home.component';
 
 import { FormsModule } from '@angular/forms';
 import { PfizerPackOrderComponent } from './pfizer-pack-order/pfizer-pack-order.component';
+import { GenerateTicketComponent } from './generate-ticket/generate-ticket.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PfizerLoginComponent,
     PfizerHomeComponent,
-    PfizerPackOrderComponent
+    PfizerPackOrderComponent,
+    GenerateTicketComponent
   ],
   imports: [
     BrowserModule,
